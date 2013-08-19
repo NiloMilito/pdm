f27a177a8fa6b5c0caa1cedbc2eb9c54565eecc6
Danilo <Danilo@Danilo-PC>
2013-08-19 08:48:23 -0300

    atribute de ambiente

--- GPSProject/src/br/edu/ifpb/model/Ambiente.java
+++ GPSProject/src/br/edu/ifpb/model/Ambiente.java
@@ -9,40 +9,37 @@ public class Ambiente {
 	private String nome;
 	private Location location;
 	private Date date;
-	
-	public Ambiente() {
-	}
-
+	private Double raio;
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public Location getLocation() {
 		return location;
 	}
-
 	public void setLocation(Location location) {
 		this.location = location;
 	}
-
 	public Date getDate() {
 		return date;
 	}
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	public Double getRaio() {
+		return raio;
+	}
+	public void setRaio(Double raio) {
+		this.raio = raio;
+	}
+
 	
 }
